db71f64c36b2f1fe715f7fd12f81832bb5a54da2
Matheus Yamanari <matheusvictormy@gmail.com>
2025-04-18 18:21:45 -0400

    teste

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-fetch('http://localhost:3000/usuarios')
-  .then(res => res.json())
-  .then(data => {
-    console.log('Dados recebidos:', data);
-    // Exibir no HTML
-  })
-  .catch(err => console.error('Erro:', err));
